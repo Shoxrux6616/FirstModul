e2ec95af998523ff9e627f33c4b5a2ce427d4df5
Shoxrux6616 <shoxruxsoliqulov@gmail.com>
2024-11-08 20:47:49 +0500

    finsh

--- srcModul/srcModul/Program.cs
+++ srcModul/srcModul/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace srcModul
+﻿namespace srcModul;
+
+public class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.Write(" son : ");
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        var ones = number % 10;
+        var tens = number / 10 % 10;
+        var hundreds = number / 100 % 10;
+        var thundreds = number / 1000;
+
+        var resault = 0;
+        if (number > 999 && number < 10000)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(ones + tens + hundreds + thundreds);
+        }
+        else
+        {
+            Console.WriteLine("$ honali son kiriting?");
         }
     }
 }
